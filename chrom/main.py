4f9317765a7098b304e08b907a56a337175174fb
GammaGVA <gri_v_a@vk.com>
2022-11-24 14:55:30 +0300

    Разобрался с None, чуть чуть улучшил запись.

--- chrom/main.py
+++ chrom/main.py
@@ -19,49 +19,58 @@ try:
     url = r'https://egrul.nalog.ru/index.html'
     browser.get(url=url)
     find_region = browser.find_element(by='name', value='query')
+
     sleep(2)
-    # name_orgs: str = input('Введите запрос на организации (например окология, хозяйство, буз): ')
-    name_orgs = 'молоко'
+
+    name_orgs: str = input('Введите запрос на организации (например окология, хозяйство, буз): ')
+    # name_orgs = 'ОТКРЫТОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО ПО ПЕРЕРАБОТКЕ МОЛОКА "КУБАРУС-МОЛОКО"'
+
     find_inn = browser.find_element(by='xpath', value='//*[@id="query"]').send_keys(name_orgs)
     # Или могли имитировать ENTER find_inn.send_keys(Keys.ENTER), но два сенда нельзя на одну кнопку, или я не понял как
     browser.find_element(by='xpath', value='//*[@id="query"]').send_keys(Keys.ENTER)
     # sleep(1)
     # button = browser.find_element(by='xpath', value='//button[@type="submit"]')
     # button.click()
-    name_orgs = name_orgs.replace(' ', '_').replace(',', '_').replace('.', '')
 
-    pagination = BS(browser.page_source, 'lxml').find_all('a', class_='lnk-page')
+    # name_orgs = name_orgs.replace(' ', '_').replace(',', '_').replace('.', '')
+    # pagination = BS(browser.page_source, 'lxml').find_all('a', class_='lnk-page')
+    # Скорее всего сайт палит что я его парсю, надо попробовать через selenium кнопки искать. Так что код выше не подходит.
+
     sleep(2)
-    number: int = int(pagination[-2].text)
-    count = 1
+    number: int = int(browser.find_elements(by='class name', value='lnk-page')[-2].text)
 
-    for number_page in range(1, 2):
+    for number_page in range(number):
+        count = 0
         sleep(1)
         soup = BS(browser.page_source, 'lxml')
-        # Слетела lxml долго не мог понять почему вчера работало а сегодня нет (pip install lxml)
+        # Слетела lxml долго не мог понять почему вчера работало, а сегодня нет (pip install lxml)
+        orgs = soup.find_all('a', class_='op-excerpt')
         all_page = soup.find_all('div', class_='res-row')
         for org in all_page:
             info_org = org.find("div", class_="res-text").text
             info_org_lst = info_org.split(',')
-            nme_org = info_org_lst[0].strip()
-            adress = ''
-            if len(info_org_lst[0].split(':')) == 2:
-                nme_org = info_org_lst[0].split(':')[0].strip()
-                adress = [info_org_lst[0].split(':')[1].strip()]
-            Dct = {'№' + str(count) + ' ' + nme_org: {}}
+            nme_org = orgs[count].text.strip()
+            # Слеши "\" не реплесются в название, пробовал по всякому не получается.
+            adress = []
+            Dct = {nme_org: {}}
             count += 1
-            for i in info_org_lst[1:]:
+            for i in info_org_lst:
                 if ':' in i:
-                    Dct[nme_org][i.split(':')[0].strip()] = i.split(':')[1].strip()
+                    point = i.split(':')[0].strip()
+                    vale = i.split(':')[1].strip()
+                    Dct[nme_org].setdefault(point, vale)
                 else:
                     adress += [i]
-                Dct[nme_org]['Адрес'] = ' '.join(adress).replace('  ', ' ').strip()
-                if 'Дата прекращения деятельности' not in org.find("div", class_="res-text").text:
-                    with open(f'{name_orgs}.json', 'a', encoding='utf-8') as file:
-                        json.dump(Dct, file)
-                else:
-                    with open(f'{name_orgs}_ликвид.json', 'a', encoding='utf-8') as file:
-                        json.dump(Dct, file, indent=4, ensure_ascii=False)
+            Dct[nme_org]['Адрес'] = ' '.join(adress).replace('  ', ' ').strip()
+            # Столько провозился и не понимал почему не всё пишется и много не полных словарей.
+            # Я запись под for запихнул.
+            if 'Дата прекращения деятельности' not in info_org:
+                with open(f'{name_orgs}.json', 'a', encoding='utf-8') as file:
+                    json.dump(Dct, file, indent=4, ensure_ascii=False)
+            else:
+                with open(f'{name_orgs}_ликвидированные.json', 'a', encoding='utf-8') as file:
+                    json.dump((Dct, '\n'), file, indent=4, ensure_ascii=False)
+            # Запись идёт криво, видимо json расчитан под запись одного словоря(скобки там в притирку получаются "конец пред словоря" - }{ - "начало след").
         browser.find_element(by='class name', value='lnk-page-next').click()
         # Тут раньше был косяк, искал по пути, и при новых значения name по страницам не переходило.
         sleep(2)
@@ -72,10 +81,18 @@ finally:
     browser.quit()
 
 '''
-В общих чертах даже удобнее чем request. На мой взгляд по крайней мере.
+В общих чертах гораздо удобнее чем request. На мой взгляд.
+
 by="class name" у меня нигде не сработал, так что скорее всего буду использовать "xpath".
+Всё заработало, руки выпримялись.
+
 Муторно что send_keys нельзя к одной переменной использовать дважды, но это просто увеличивает количество строк.
+
 Не захотел дрочиться с текстом, вначале думал сделать json но у всех адрес разбит по разному.
-У когото нет запятых в адресе у кагото есть, рещил просто азы пока узнать и не ломать голову с группировкой данных.
+Сделал json. Дальше попробовать в БД записать.
+
+У кого-то нет запятых в адресе у кого-то есть, решил просто азы пока узнать и не ломать голову с группировкой данных.
+
 Как же задолбала подмена данных на None. Всё же selenium не панацея, но я и сам виноват. Долблю по 100500 раз сервер.
+А нет с подменой разобрался, всё таки панацея.
 '''
